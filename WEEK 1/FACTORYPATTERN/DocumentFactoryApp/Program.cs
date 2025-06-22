672505e0321998cb8df9a636bfd07e18b123f60c
Dhalmi44 <dhalmidhana7@gmail.com>
2025-06-22 18:00:09 +0530

    WEEK 1

---
+++ WEEK 1/FACTORYPATTERN/DocumentFactoryApp/Program.cs
@@ -0,0 +1,100 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+#region Product Interface
+interface IDocument
+{
+    void Open();
+}
+#endregion
+
+#region Concrete Products
+class WordDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening a Word document.");
+    }
+}
+
+class PdfDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening a PDF document.");
+    }
+}
+
+class ExcelDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening an Excel document.");
+    }
+}
+#endregion
+
+#region Factory Base
+abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+#endregion
+
+#region Concrete Factories
+class WordFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+#endregion
+
+#region Main
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter document type to open (Word / PDF / Excel): ");
+        string input = Console.ReadLine().Trim().ToLower();
+
+        DocumentFactory factory = null;
+
+        switch (input)
+        {
+            case "word":
+                factory = new WordFactory();
+                break;
+            case "pdf":
+                factory = new PdfFactory();
+                break;
+            case "excel":
+                factory = new ExcelFactory();
+                break;
+            default:
+                Console.WriteLine("Invalid document type entered.");
+                return;
+        }
+
+        IDocument document = factory.CreateDocument();
+        document.Open();
+    }
+}
+#endregion
---
+++ WEEK 1/SINGLETON/AppLoggerApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class MyLogger
+{
+    private static MyLogger instance;
+    private static readonly object lockObj = new object();
+
+    private MyLogger()
+    {
+        Console.WriteLine("====== MyLogger instance created ======");
+    }
+
+    public static MyLogger GetInstance()
+    {
+        if (instance == null)
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                {
+                    instance = new MyLogger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("LOG: " + message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // First logger instance
+        MyLogger logger1 = MyLogger.GetInstance();
+        Console.WriteLine("Enter the first log message:");
+        string firstMessage = Console.ReadLine();
+        logger1.Log(firstMessage);
+
+        // Second logger instance
+        MyLogger logger2 = MyLogger.GetInstance();
+        Console.WriteLine("Enter the second log message:");
+        string secondMessage = Console.ReadLine();
+        logger2.Log(secondMessage);
+
+        // Check if they are the same instance
+        if (ReferenceEquals(logger1, logger2))
+        {
+            Console.WriteLine("\n===== Same MyLogger instance used =====");
+        }
+        else
+        {
+            Console.WriteLine("\n===== Different MyLogger instances created! =====");
+        }
+    }
+}
